fc1f92a5a4282a0ca301eca1da4953c7e858f332
acleal <andrew@emteq.net>
2023-02-07 14:49:02 +0000

    first commit
    first commit
    includes zip for swift binary package

--- Package.swift
+++ Package.swift
@@ -3,26 +3,28 @@
 
 import PackageDescription
 
+
+let version = "1.0.0"
+let moduleName = "ocoExpEngSBP"
+//let checksum = "8f42f67a0665798e1c4e149c73a36fb9dddc062bc90e3b4e077ea571613ddda1"
+let checksum = "4509a23656f8d1a080f45d3996e1ba504281454690b03bc5d8c32f58704045a3"
+
 let package = Package(
-    name: "ocoExpEngSBP",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ocoExpEngSBP",
-            targets: ["ocoExpEngSBP"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ocoExpEngSBP",
-            dependencies: []),
-        .testTarget(
-            name: "ocoExpEngSBPTests",
-            dependencies: ["ocoExpEngSBP"]),
-    ]
+	name: moduleName,
+	platforms: [
+		.iOS(.v13) //, .macCatalyst(.v15), .macOS(.v10_15)
+	],
+	products: [
+		.library(
+			name: moduleName,
+			targets: [moduleName]
+		)
+	],
+	targets: [
+		.binaryTarget(
+			name: moduleName,
+			url: "https://github.com/litoarias/NetworkModuleDistribution/releases/download/\(version)/\(moduleName).xcframework.zip",
+			checksum: checksum
+		)
+	]
 )
--- Sources/ocoExpEngSBP/ocoExpEngSBP.swift
+++
@@ -1,6 +0,0 @@
-public struct ocoExpEngSBP {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
